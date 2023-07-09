475ca2c6bdb164a344db62f901b8e40e91b442d1
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-07-09 23:07:40 +0200

    React under the hook

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const Paragraph = () => {
+  return React.createElement(
+    'p',
+    {
+      id: 'first-paragraph',
+      className: 'paragraph',
+      style: {
+        backgroundColor: 'blue',
+        fontWeight: 'bold',
+      }
+    },
+    'Lorem ipsum dolor sit amet'
+  );
+};
+
+const appContainer = document.querySelector('#app');
+
+ReactDOM.render(React.createElement(Paragraph), appContainer);
